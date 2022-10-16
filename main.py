93054b2796f5e389a77cfb8fea79c02071ec7e27
Luiz Miguel dos Santos Gonçalves <lm4academy@gmail.com>
2022-10-16 00:40:43 -0300

    getting all titles

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import wikipediaapi
 
+class Title:
+    def __init__(self, title: str, text: str, level: int) -> None:
+        self.title = title
+        self.level = level
+        self.text = text
+    
+    def __str__(self):
+        return f'{self.title}: {self.text[:40]}...'
+
+TITLE_LIST = []
+
+def decode(sections: wikipediaapi.WikipediaPageSection, level=0):
+    for section in sections:
+        TITLE_LIST.append(Title(section.title, section.text, level))
+        decode(section.sections, level+1)
+
+
 article_name = input("Qual artigo da wikipedia você quer ler?\n: ")
 
 wiki_wiki = wikipediaapi.Wikipedia('pt')
@@ -11,5 +28,13 @@ if page.exists():
     print("Seu sumário é: \n"+page.summary[:280], end='')
     if len(page.summary) > 60:
         print("...")
+    print("\n-----\n")
+    print(f"Sua url é: {page.fullurl}")
+    print("\n-----\n")
+    print("Suas seções são:")
+    decode(page.sections)
+    for title in TITLE_LIST:
+        print("\t"*title.level,title)
+
 else:
     print("A página não existe.")
